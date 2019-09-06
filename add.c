411bbcb17c7b82e303145f01a419e29a0b3fedb6
Siddharthvals <mailtosiddharthg@gmail.com>
2019-09-06 19:15:52 +0530

    addition without using a+b expression

---
+++ add.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int x,y ; 
+	printf("enter the two number to add"); 
+	scanf("%d%d",&x,&y); 
+	while(y--!=0)
+{
+	x++;
+	//y--; 
+}
+	printf("the result of sum is %d:",x); 
+return 0; 
+	}
