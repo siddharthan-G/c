9c5dd847f3b58359be0bf269e4ad5cc60fafaa7f
Siddharthvals <mailtosiddharthg@gmail.com>
2019-09-06 19:18:07 +0530

    simple array

---
+++ array.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+void display(int *q,int ,int);
+void show (int (*q)[4],int,int);
+void print(int[][4],int, int);
+int main()
+{
+    int a[3][4]={
+        1,2,3,4,
+        5,6,7,8,
+        9,0,1,2
+    };
+    display (a,3,4);
+    show (a,3,4);
+    print (a,3,4);
+    return 0;
+    
+}
+void display(int *q, int row,int col)
+{
+    int i,j; 
+    for(i=0;i<row ;i++)
+    {
+        for(j=0;j<col;j++)
+            printf("%d",*(q+i* col+j));
+        printf("\n");
+    }
+    printf("\n");
+    
+}
+void show(int (*q)[4], int row,int col)
+{
+    
+    int i ,j  ; 
+    int *p ; 
+    for(i =0;i<row;i++)
+    {
+        p=q+i; 
+        for(j=0;j<col;j++)
+        printf("%d",*(p+j));
+        printf("\n");
+        
+    }
+    printf("\n");
+}
+void print(int q[][4],int row ,int col)
+{
+    
+    int i,j; 
+    for(i=0;i<row;i++)
+    {
+        for(j=0;j<col;j++)
+            printf("%d",q[i][j]);
+            printf("\n");
+    
+       // printf("\n");
+    }
+}
